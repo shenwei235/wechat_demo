30e23b2b01a3bc7e59508222ccd7883547f4e4da
Shen Wei <shenwei235@gmail.com>
2015-01-26 13:54:32 +0800

    debug

--- app/controllers/omniauth_wechat_callbacks_controller.rb
+++ app/controllers/omniauth_wechat_callbacks_controller.rb
@@ -1,9 +1,11 @@
 class OmniauthWechatCallbacksController < ApplicationController
 
   def failure
+    puts "xxxxx"
     puts request.headers
     puts request.env["omniauth.auth"]
-    redirect_to "/qy_apps"
+    puts "xxxxx"
+    # redirect_to "/qy_apps"
   end
 
 end
