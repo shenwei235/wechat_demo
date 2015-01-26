795aa4dd202d10c7765e4f6d3293a3a9ae2d7047
Shen Wei <shenwei235@gmail.com>
2015-01-26 16:09:56 +0800

    debug

--- app/controllers/omniauth_wechat_callbacks_controller.rb
+++ app/controllers/omniauth_wechat_callbacks_controller.rb
@@ -1,6 +1,16 @@
 require 'net/http'
 class OmniauthWechatCallbacksController < ApplicationController
 
+  def start
+    redirect_to "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{WECHAT_CONFIG['WECHAT_APP_ID']}&redirect_uri=http%3A%2F%2Fviduapp.com%2Fauth%2Fwechat%2Fcallback&response_type=code&scope=snsapi_userinfo&state=8e55d93c2a3fe584c14382def8a99bcff15131db64cbba58&connect_redirect=1#wechat_redirect"
+  end
+
+  def callback
+    Rails.logger.info "zzzzz"
+    Rails.logger.info request
+    Rails.logger.info "zzzzz"
+  end
+
   def failure
     Rails.logger.info "xxxxx"
     Rails.logger.info request.headers.to_json
