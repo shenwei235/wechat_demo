adcc2aa73b3a0418c6409df2e96c7038b8f712f0
Shen Wei <shenwei235@gmail.com>
2015-01-26 15:13:26 +0800

    debug

--- app/controllers/omniauth_wechat_callbacks_controller.rb
+++ app/controllers/omniauth_wechat_callbacks_controller.rb
@@ -3,14 +3,17 @@ class OmniauthWechatCallbacksController < ApplicationController
 
   def failure
     Rails.logger.info "xxxxx"
-    Rails.logger.info request.headers
-    Rails.logger.info request.env["omniauth.auth"]
+    Rails.logger.info request.headers.to_json
+    Rails.logger.info request.env.to_json
     Rails.logger.info "xxxxx"
     # redirect_to "/qy_apps"
   end
 
   def setup
-    request.env['omniauth.strategy'].options[:client_secret] = get_access_token
+    Rails.logger.info "zzzzz"
+    Rails.logger.info request.env.to_json
+    Rails.logger.info "zzzzz"
+    # request.env['omniauth.strategy'].options[:client_secret] = get_access_token
   end
 
   private
