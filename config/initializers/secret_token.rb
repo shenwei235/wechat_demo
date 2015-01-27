9b9c223e33d94c199d661dcf0d098a5cf938d0fe
Shen Wei <shenwei235@gmail.com>
2015-01-27 16:15:37 +0800

    add devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QyWechat::Application.config.secret_token_base = '6a4d9be89b4b82468a0c99a5e457d3e80b7cda75f7be4b97c8858b7d317a28228fece75bccaeee39bf73e1e790fb02db539f95af2ed238e55a1a4d9be89b4b82'
