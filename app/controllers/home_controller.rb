710f167327dd22c9534d238e4c68423bffdf7d50
Shen Wei <shenwei235@gmail.com>
2015-01-27 09:30:47 +0800

    debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,10 +11,14 @@ class HomeController < ApplicationController
   end
 
   def callback
-    if params[:code]
+    if params[:code] && @group_client.is_valid?
+      Rails.logger.info params[:code]
       @user_info = @group_client.oauth.get_user_info(params[:code], '1')
+      Rails.logger.info @user_info["result"]["UserId"]
+      @user_profile = @group_client.user.get(@user_info["result"]["UserId"])
       Rails.logger.info @user_info.to_json
-      render text: @user_info.to_json, status: 200
+      Rails.logger.info @user_profile.to_json
+      render text: [@user_info, @user_pofile].to_json, status: 200
     end
   end
 
